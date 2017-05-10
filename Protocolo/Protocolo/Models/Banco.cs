3cb8e0ea0f085fe41fb2d371ec8d3d8da0a3f0d0
Igor <igorbaesse86@gmail.com>
2017-05-10 16:58:21 -0300

    v0.1

---
+++ Protocolo/Protocolo/Models/Banco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Protocolo.Models
+{
+    public class Banco
+    {
+        public static Boolean GetConexao()
+        {
+            SqlConnection conexa = new SqlConnection("Server=tcp:cartoriojaguarao.database.windows.net,1433;Initial Catalog=BDCARTORIO;Persist Security Info=False;User ID=rootacesso;Password=75395146@carbono;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            try
+            {
+                conexa.Open();
+                return true;
+
+            }
+            catch (SqlException e)
+
+            {
+                e.ToString();
+                return false;
+
+            }
+           
+            
+        }
+    }
+}
---
+++ Protocolo/Protocolo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Protocolo.Startup))]
+namespace Protocolo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
